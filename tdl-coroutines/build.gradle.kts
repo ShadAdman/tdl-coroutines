6b0355f5fc2e1ea3945d4412251a1a2e1ffe2a9e
Georgii Ippolitov <github@g000sha256.dev>
2026-05-17 16:18:08 +0300

    Raise JVM target to 11

--- tdl-coroutines/build.gradle.kts
+++ tdl-coroutines/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     defaultConfig {
@@ -58,14 +58,14 @@ kotlin {
         publishLibraryVariants("release")
 
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_1_8
+            jvmTarget = JvmTarget.JVM_11
             moduleName = packageName
         }
     }
 
     jvm {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_1_8
+            jvmTarget = JvmTarget.JVM_11
             moduleName = packageName
         }
     }
