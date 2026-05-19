cc64bfe9f58a49f6fb30b546ca5cfa4d0f201504
Georgii Ippolitov <github@g000sha256.dev>
2026-05-19 21:44:26 +0300

    Fix invalid temp directory prefix on JVM

--- tdl-coroutines/src/jvmMain/kotlin/dev/g000sha256/tdl/TdlSystemLibrary.kt
+++ tdl-coroutines/src/jvmMain/kotlin/dev/g000sha256/tdl/TdlSystemLibrary.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Georgii Ippolitov (g000sha256)
+ * Copyright 2025-2026 Georgii Ippolitov (g000sha256)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,7 +105,9 @@ private fun load(platform: String, libraryName: String) {
 }
 
 private fun load(platform: String, libraryNames: List<String>) {
-    createTempDirectory(prefix = platform)
+    platform
+        .replace(oldChar = '/', newChar = '-')
+        .let { prefix -> createTempDirectory(prefix = prefix) }
         .toFile()
         .also { directory -> directory.deleteOnExit() }
         .also { directory -> load(libraryNames = libraryNames, directory = directory, platform = platform) }
